36b6d5f6de0cbe157eebe839712a645897c6c647
unknown <christykyang@gmail.com>
2020-03-09 12:08:44 -0500

    init

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+            List<string> withTH = words.FindAll(w => w.Contains("th"));
+            foreach (string word in withTH)
+            {
+                Console.WriteLine(word);
+            }
+            Console.ReadLine();
+        }
+    }
+}
