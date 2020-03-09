e1a104e4b63136df206b0a5a664e6bbad562185a
unknown <christykyang@gmail.com>
2020-03-09 14:53:58 -0500

    Completed LINQ problem two. Run and works.

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace LINQ
     {
         static void Main(string[] args)
         {
+            //problem 1
             List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
             List<string> withTH = words.FindAll(w => w.Contains("th"));
             foreach (string word in withTH)
@@ -17,6 +18,32 @@ namespace LINQ
                 Console.WriteLine(word);
             }
             Console.ReadLine();
+
+            //problem 2
+            List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike" };
+            IEnumerable<string> distinctNames = names.Distinct();
+            foreach(string name in distinctNames)
+            {
+                Console.WriteLine(name);
+            }
+            Console.ReadLine();
+
+            //problem 3
+            int intIndex;
+            List<int> gradesInNumbers = new List<int>();
+            List<string> classGrades = new List<string>()
+            {
+                "80,100,92,89,65",
+                "93,81,78,84,69",
+                "73,88,83,99,64",
+                "98,100,66,74,55"
+            };
+            //gradesInNumbers = 
+            //int[] grades = intIndex.Split()
+            
+
+
         }
+
     }
 }
